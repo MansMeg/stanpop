db5ece0af6192c01e9f944ead8c26775152b54e7
Måns Magnusson <mons.magnusson@gmail.com>
2026-04-10 08:15:22 +0200

    add internals

--- tests/testthat/helper-internals.R
+++ tests/testthat/helper-internals.R
@@ -3,3 +3,7 @@
 get_internal <- function(name, pkg = "stanpop") {
   getFromNamespace(name, pkg)
 }
+
+sampler_state_test_stan_file <- function() {
+  testthat::test_path("stan_code", "sampler_state_test.stan")
+}
