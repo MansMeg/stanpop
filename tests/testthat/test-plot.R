940db009f7c8cea2f79408f3def2d497aedf94ed
Måns Magnusson <mons.magnusson@gmail.com>
2026-04-21 08:13:37 +0200

    Add unit test

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,18 +1,14 @@
 context("plot")
 
-test_that("plot works for polls data object", {
+test_that("plot works for saved poll_of_polls fixture", {
+  pop <- load_pop(testthat::test_path("files", "test_pop_8m5_cmdstanr.rds"))
 
-  skip("TODO: Test that there is a warning if not the whole latent state is plotted that also propose how to change time_range to show the whole LS")
-
-
-})
-
-
-
-test_that("plot works for poll_of_polls data object", {
+  expect_s3_class(pop, "poll_of_polls")
+  expect_silent(plt <- suppressWarnings(plot(pop, pop$y[1])))
+  expect_s3_class(plt, "ggplot")
+  expect_silent(suppressWarnings(ggplot2::ggplot_build(plt)))
 
   skip("TODO: Test that there is a warning if not the whole latent state is plotted that also propose how to change time_range to show the whole LS")
 
 })
-
 
