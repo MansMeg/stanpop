68f76fc9cc63d93376c211864a1d59a88ce45518
Måns Magnusson <mons.magnusson@gmail.com>
2026-05-10 21:15:44 +0200

    added checked tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -111,6 +111,14 @@ test_that("latent-state display dates can use period-start anchor dates", {
   expect_equal(unname(display_dates), tl$time_line$date, tolerance = 0)
 })
 
+test_that("latent-state period-end display dates handle weekly timelines", {
+  latent_state_display_dates <- get_internal("latent_state_display_dates")
+  tl <- time_line(time_range(c("2014-09-01", "2014-09-21")), time_scale = "week")
+
+  display_dates <- latent_state_display_dates(tl, position = "period_end")
+
+  expect_equal(unname(display_dates), tl$time_line$date + lubridate::days(6), tolerance = 0)
+})
 test_that("plot works for polls data object", {
   skip("TODO: Test that there is a warning if not the whole latent state is plotted that also propose how to change time_range to show the whole LS")
 })
